7f2835b4a9f1be4012a3136733d8f0616c6398c8
KimTaeGwan <ab154@naver.com>
2019-11-22 20:22:29 +0900

    Edit style

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const Layout = () => {
+const Layout = props => {
+  const { children } = props;
   return (
     <div id="layout" className="layout">
       <div id="container" className="container">
