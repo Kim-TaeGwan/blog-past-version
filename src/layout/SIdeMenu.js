5ac49ddcfad06e2d22315100ea87ec63b832aba6
TaeGwan Kim <taegwankim@TaeGwanui-MacBookPro.local>
2020-01-19 18:26:15 +0900

    Add layout or sidemenu

--- src/layout/SIdeMenu.js
+++ src/layout/SIdeMenu.js
@@ -1,7 +1,48 @@
 import React from "react";
 
-const SIdeMenu = () => {
-  return <div>SideMenu</div>;
+const SideMenu = () => {
+  return (
+    <div className="sideMenu">
+      <div className="profile_img">
+        <img alt="profile_img" width="100%" height="100%" />
+      </div>
+      <div className="title">Portfolio</div>
+      <div className="noti">Template by W3.CSS</div>
+      <div>
+        <ul>
+          <li>
+            <span>
+              <img alt="side_menu_ico" width="100%" height="100%" />
+            </span>
+            <span>
+              PORTFOLIO
+            </span>
+          </li>
+          <li>
+            <span>
+              <img alt="side_menu_ico" width="100%" height="100%" />
+            </span>
+            <span>
+              ABOUT
+            </span>
+          </li>
+          <li>
+            <span>
+              <img alt="side_menu_ico" width="100%" height="100%" />
+            </span>
+            <span>
+              CONTACT
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="sns_list">
+        <span><img alt="" width="100%" height="100%" /></span>
+        <span><img alt="" width="100%" height="100%" /></span>
+        <span><img alt="" width="100%" height="100%" /></span>
+      </div>
+    </div>
+  );
 };
 
-export default SIdeMenu;
+export default SideMenu;
