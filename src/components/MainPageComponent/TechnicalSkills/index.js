eca5be346f05da95e69cbf777faa05b86be64b42
KimTaeGwan <ab154@naver.com>
2020-02-08 03:30:08 +0900

    Add technical skills

--- src/components/MainPageComponent/TechnicalSkills/index.js
+++ src/components/MainPageComponent/TechnicalSkills/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PartTitle from '../../common/PartTitle';
+import SkillsBar from './SkillsBar';
 
 const TechnicalSkills = () => {
 	return (
 		<div className="technical_skills">
 			<PartTitle>Technical Skills</PartTitle>
+			<SkillsBar skillTitle="Photography" percent="95%" style={{ width: '95%' }} />
+			<SkillsBar skillTitle="Web Design" percent="85%" style={{ width: '85%' }} />
+			<SkillsBar skillTitle="PhotoShop" percent="80%" style={{ width: '80%' }} />
 		</div>
 	);
 };
