baa11c5812bcf53f3372962eeff25daa45206c0f
KimTaeGwan <ab154@naver.com>
2020-02-08 23:36:00 +0900

    Add contact_information

--- src/components/MainPageComponent/TechnicalSkills/index.js
+++ src/components/MainPageComponent/TechnicalSkills/index.js
@@ -9,6 +9,7 @@ const TechnicalSkills = () => {
 			<SkillsBar skillTitle="Photography" percent="95%" style={{ width: '95%' }} />
 			<SkillsBar skillTitle="Web Design" percent="85%" style={{ width: '85%' }} />
 			<SkillsBar skillTitle="PhotoShop" percent="80%" style={{ width: '80%' }} />
+			<hr />
 		</div>
 	);
 };
