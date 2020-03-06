eeb497accdcaf2af5bdabc0124a9f45f441a3dd2
KimTaeGwan <ab154@naver.com>
2020-03-07 02:57:04 +0900

    Add router hashrouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Main from './pages/Main';
-import StudyTable from './pages/studytable';
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Main from "./pages/Main";
+import StudyTable from "./pages/studytable";
 
 const Routes = () => {
-	return (
-		<Switch>
-			<Route exact path="/" component={Main} />
-			<Route path="/studyTable" component={StudyTable} />
-		</Switch>
-	);
+  return (
+    <HashRouter basename="/">
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/studyTable" component={StudyTable} />
+      </Switch>
+    </HashRouter>
+  );
 };
 
 export default Routes;
