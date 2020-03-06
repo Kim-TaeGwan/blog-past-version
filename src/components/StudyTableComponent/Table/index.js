ba088854b869440f9258111f34c2324032a78888
KimTaeGwan <ab154@naver.com>
2020-03-07 00:48:15 +0900

    Edit table style

--- src/components/StudyTableComponent/Table/index.js
+++ src/components/StudyTableComponent/Table/index.js
@@ -6,45 +6,30 @@ const TableComponent = () => {
     <div className="table_container">
       <PartTitle>StudyTable</PartTitle>
       <table>
-        {/* <thead>
-          <tr>
-            <th>No</th>
-            <th>제 목</th>
-            <th>날 짜</th>
-          </tr>
-        </thead> */}
         <tbody>
           <tr>
             <td>01</td>
-            <td>test1test1test1test1test1test1test1test1test1</td>
+            <td className="title">test1test1test1test1</td>
             <td>20.02.28</td>
           </tr>
           <tr>
             <td>01</td>
-            <td>
-              test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2test2
-            </td>
+            <td className="title">test2test2test2test2test2</td>
             <td>20.02.28</td>
           </tr>
           <tr>
             <td>01</td>
-            <td>
-              test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3
-            </td>
+            <td className="title">test3test3</td>
             <td>20.02.28</td>
           </tr>
           <tr>
             <td>01</td>
-            <td>
-              test4test4test4test4test4test4test4test4test4test4test4test4test4test4test4test4test4test4
-            </td>
+            <td className="title">test4test4</td>
             <td>20.02.28</td>
           </tr>
           <tr>
             <td>01</td>
-            <td>
-              test5test5test5test5test5test5test5test5test5test5test5test5test5test5test5
-            </td>
+            <td className="title">test5test5</td>
             <td>20.02.28</td>
           </tr>
         </tbody>
