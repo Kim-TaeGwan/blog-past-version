20397bd4245c230fc58f82c9f968147dbe806121
KimTaeGwan <ab154@naver.com>
2020-03-31 18:21:11 +0900

    Edit portpolio_list

--- src/components/common/Portfolio.js
+++ src/components/common/Portfolio.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import mountains_img from '../../assets/image/mountains.jpg';
+import mountains_img from "../../assets/image/mountains.jpg";
 
-const Portfolio = () => {
-	return (
-		<div className="portfolio">
-			<div className="portfolio_img">
-				<img src={mountains_img} alt="portfolio_img" width="100%" />
-			</div>
-			<div className="portfolio_info">
-				<p>
-					<b>Lorem Ipsum</b>
-				</p>
-				<p>
-					Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies
-					congue gravida diam non fringilla.
-				</p>
-			</div>
-		</div>
-	);
+const Portfolio = ({ key }) => {
+  return (
+    <div className="portfolio" key={key}>
+      <div className="portfolio_img">
+        <img src={mountains_img} alt="portfolio_img" width="100%" />
+      </div>
+      <div className="portfolio_info">
+        <p>
+          <b>Lorem Ipsum</b>
+        </p>
+        <p>
+          Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum,
+          porta lectus vitae, ultricies congue gravida diam non fringilla.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
