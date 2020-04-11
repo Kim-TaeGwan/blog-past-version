2bf948eab68113257d2f2812dbca0ecd4f71569e
KimTaeGwan <ab154@naver.com>
2020-04-11 16:33:08 +0900

    Add portfolio api

--- src/components/MainPageComponent/PortfolioList/index.js
+++ src/components/MainPageComponent/PortfolioList/index.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
-import Portfolio from "../../common/Portfolio";
-import PageList from "../../common/PageList";
+import React, { useState, useEffect } from "react";
+import Portfolio from "components/common/Portfolio";
+import PageList from "components/common/PageList";
+
+import { portfolioApi } from "shared/Api";
+import { Link } from "react-router-dom";
 
 // const portfolioList = [1, 2, 3, 4];
 const portfolioList = [
@@ -9,24 +12,50 @@ const portfolioList = [
     comment: "첫번째 작업물, 반응형 X",
     img_url:
       "https://elric-blog.s3.ap-northeast-2.amazonaws.com/PortfoiloThumbnail/firstPortFolio.PNG",
-    url: "http://elric.dothome.co.kr/"
-  }
+    url: "http://elric.dothome.co.kr/",
+  },
 ];
 
 const PortfolioList = () => {
-  const [list, setList] = useState(portfolioList);
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchPortfolioList() {
+      setIsLoading(true);
+      try {
+        const result = await portfolioApi.get("/");
+
+        // console.log(result);
+
+        setList(result.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchPortfolioList();
+  }, []);
+
+  console.log(list);
+
   return (
     <>
       <div className="portfolio_list">
-        {list.map((list, i) => (
-          <Portfolio
-            key={i}
-            title={list.title}
-            comment={list.comment}
-            img_url={list.img_url}
-            url={list.url}
-          />
-        ))}
+        {isLoading && <div>로더</div>}
+        {list &&
+          list.map((item, i) => (
+            <a key={i} href={item.url}>
+              <Portfolio
+                title={item.title}
+                comment={item.comment}
+                img_url={item.img_url}
+                url={item.url}
+              />
+            </a>
+          ))}
       </div>
       <PageList />
     </>
--- src/shared/Api.js
+++ src/shared/Api.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 // IE에서 axios 통신 결과 캐싱되는 현상 방지
 axios.defaults.headers.common = {
-  Pragma: "no-cache"
+  Pragma: "no-cache",
 };
 
+const BACKEND_URL = "http://127.0.0.1:4000";
+// console.log(process.env);
+
 export const mainApi = axios.create({
-  baseURL: `${process.env.REACT_BACKEND_URL}/main`
+  baseURL: `${BACKEND_URL}/main`,
 });
 
 export const portfolioApi = axios.create({
-  baseURL: `${process.env.REACT_BACKEND_URL}/portfolio`
+  baseURL: `${BACKEND_URL}/api/portfolio`,
 });
 
 export const studyApi = axios.create({
-  baseURL: `${process.env.REACT_BACKEND_URL}/study`
+  baseURL: `${BACKEND_URL}/study`,
 });
