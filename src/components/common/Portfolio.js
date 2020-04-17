6f0b089201972362aa6edf1c3a92f0cd448335a0
KimTaeGwan <ab154@naver.com>
2020-04-17 14:07:04 +0900

    Edit portfolio style

--- src/components/common/Portfolio.js
+++ src/components/common/Portfolio.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-import mountains_img from "../../assets/image/mountains.jpg";
-
 const Portfolio = ({ title, comment, img_url }) => {
   return (
     <div className="portfolio">
       <div className="portfolio_img">
-        {/* <img src={mountains_img} alt="portfolio_img" width="100%" /> */}
         <img src={img_url} alt="portfolio_img" width="100%" />
       </div>
       <div className="portfolio_info">
