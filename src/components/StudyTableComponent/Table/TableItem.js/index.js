375dc0a4475690a625f1fb4433414eeff1ecaf25
TaeGwan Kim <taegwankim@TaeGwanui-MacBookPro.local>
2020-06-19 16:36:49 +0900

    Edit study table

--- src/components/StudyTableComponent/Table/TableItem.js/index.js
+++ src/components/StudyTableComponent/Table/TableItem.js/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const TableItem = ({ title, date, number }) => {
+const TableItem = ({ title, date, number, url }) => {
+  const notionUrl = () => {
+    let openNewWindow = window.open("about:blank");
+    openNewWindow.location.href = `${url}`;
+
+  }
   return (
-    <tr>
+    <tr onClick={notionUrl} className={`as`}>
       <td>{number}</td>
       <td className="title">{title}</td>
       <td>{date}</td>
