cfc4ae6f2d0322c4ab1cd1777e31532347e1ca76
TaeGwan Kim <taegwankim@MacBook-Pro.local>
2020-06-26 18:44:41 +0900

    Edit technical skills percent

--- src/components/MainPageComponent/TechnicalSkills/index.js
+++ src/components/MainPageComponent/TechnicalSkills/index.js
@@ -6,9 +6,9 @@ const TechnicalSkills = () => {
 	return (
 		<div className="technical_skills">
 			<PartTitle>Technical Skills</PartTitle>
-			<SkillsBar skillTitle="Photography" percent="95%" style={{ width: '95%' }} />
-			<SkillsBar skillTitle="Web Design" percent="85%" style={{ width: '85%' }} />
-			<SkillsBar skillTitle="PhotoShop" percent="80%" style={{ width: '80%' }} />
+			<SkillsBar skillTitle="Front-End" percent="65%" style={{ width: '65%' }} />
+			<SkillsBar skillTitle="Back-End" percent="35%" style={{ width: '35%' }} />
+			{/* <SkillsBar skillTitle="PhotoShop" percent="80%" style={{ width: '80%' }} /> */}
 			<hr />
 		</div>
 	);
