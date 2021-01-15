139e1537bcd4b92ca0bc32ce5b4dfbfa7471c0c7
kimtaegwan <ab154@naver.com>
2021-01-15 18:25:57 +0900

    Edit folder tree

--- src/pages/studytable/index.js
+++ src/pages/studytable/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import StudyTableContainer from '../../containers/StudyTableContainer';
+import React from "react";
+import StudyTableContainer from "containers/StudyTableContainer";
 
 const StudyTable = () => {
-	return <StudyTableContainer />;
+  return <StudyTableContainer />;
 };
 
 export default StudyTable;
