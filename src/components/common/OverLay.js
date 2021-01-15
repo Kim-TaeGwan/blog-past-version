3f682a6d3bd41693c79c419ea578a3a06c686ea5
kimtaegwan <ab154@naver.com>
2021-01-15 20:51:18 +0900

    Edit sidemenu

--- src/components/common/OverLay.js
+++ src/components/common/OverLay.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-const OverLay = () => {
-	return <div className="overlay" id="overLay" />;
+const OverLay = ({ style }) => {
+  return <div className="overlay" style={style} />;
 };
 
 export default OverLay;
