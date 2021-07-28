454806342f7dbfa27b37862a5993c6cb68d203ea
kimtaegwan <ab154@naver.com>
2021-07-28 21:43:04 +0900

    Edit pageNation

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,28 +1,10 @@
 import React, { FC } from 'react';
 
-interface Props {
-  postsPerpage: any;
-  totalPosts: any;
-  paginate: (e: any) => void;
-}
-const Pagination: FC<Props> = ({ postsPerpage, totalPosts, paginate }) => {
-  // const [insert, setInsert] = useState(false);
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerpage); i++) {
-    pageNumbers.push(i);
-  }
-  // const handlePage = number => {
-  //   setInsert(!insert);
-  //   paginate(number);
-  // };
+const Pagination: FC = () => {
   return (
     <div className="page_list">
       <div className="arrow">&lt;&lt;</div>
-      {pageNumbers.map((number) => (
-        <div key={number} className={`page insert`} onClick={() => paginate(number)}>
-          {number}
-        </div>
-      ))}
+      <div className={`page insert`}>1</div>
       <div className="arrow">&gt;&gt;</div>
     </div>
   );
