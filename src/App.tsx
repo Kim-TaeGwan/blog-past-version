621b6037dff16b47c4b8ab8dc59d37f074a7de61
kimtaegwan <ab154@naver.com>
2022-01-30 23:00:28 +0900

    Edit cors error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-import Routes from "./Routes";
+import Routes from './Routes';
+import axios from 'axios';
 
 function App() {
+  axios.defaults.withCredentials = true;
   return (
     <BrowserRouter>
       <Routes />
